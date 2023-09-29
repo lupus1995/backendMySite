78fa5e061c4a7961fe52dc5af76ad7bd8316316c
Панфилов Александр Юрьевич <canya.pafilov.95@gmail.com>
2023-09-29 23:38:54 +0300

    добавил правило валидации авторизации для модуля вебсокетов

---
+++ src/auth/rules/web-sockets/password.rule.ts
@@ -0,0 +1,41 @@
+import { Injectable, Logger } from '@nestjs/common';
+import {
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import * as argon2 from 'argon2';
+import { AuthWebsocketsFindDataService } from 'src/auth/services/auth-web-sockets/auth-web-sockets-find-data.service';
+
+@ValidatorConstraint({ name: 'check password websockets', async: true })
+@Injectable()
+export class PasswordRule implements ValidatorConstraintInterface {
+  constructor(
+    private authFindData: AuthWebsocketsFindDataService,
+    private logger: Logger,
+  ){}
+  async validate(
+    value: string,
+    validationArguments?: ValidationArguments,
+  ): Promise<boolean> {
+    const username = await this.authFindData.findByUsername({
+      username: validationArguments.object['usernameOrEmail'],
+    });
+    const email = await this.authFindData.findByEmail({
+      email: validationArguments.object['usernameOrEmail'],
+    });
+
+    const password = username?.password || email?.password;
+
+    const result = await argon2.verify(password, value);
+
+    if (!result) {
+      this.logger.error('Пароль введен неверно');
+    }
+
+    return result;
+  }
+  defaultMessage?(): string {
+    return 'Email, username или пароль введены неверно!';
+  }
+}
