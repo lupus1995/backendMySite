bd81755803d275f94943ce4d2e10e8a31232b7b3
Панфилов Александр Юрьевич <canya.pafilov.95@gmail.com>
2023-09-30 10:44:10 +0300

    вынес сервисы правила валидации модуля сокетов в отдельный модуль

---
+++ src/auth/rules/web-sockets/websockets-rule.module.ts
@@ -0,0 +1,20 @@
+import { Logger, Module } from '@nestjs/common';
+import { AuthWebSocketsModule } from 'src/auth/services/auth-web-sockets/auth-web-sockets.module';
+import { PasswordRule } from './password.rule';
+import { UniqEmailRule } from './uniq-email.rule';
+import { UniqUsernameRule } from './uniq-username.rule';
+import { UsernameOrEmailRule } from './username-or-email.rule';
+
+@Module({
+  imports: [AuthWebSocketsModule],
+  controllers: [],
+  providers: [
+    Logger,
+    PasswordRule,
+    UniqEmailRule,
+    UniqUsernameRule,
+    UsernameOrEmailRule,
+  ],
+  exports: [PasswordRule, UniqEmailRule, UniqUsernameRule, UsernameOrEmailRule],
+})
+export class WebsocketsRuleModule {}
